79708db6947fb9d8aa120cdcd408cf9816c28482
Kristine Marie Baluyot <kmbaluyo@ucsd.edu>
2020-02-12 17:24:06 -0800

    ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,19 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get('/project/' + idNumber, addProject);
+	
 }
+
+
+function addProject(result) {
+	  var projectHTML = '<a href="#" class="thumbnail">' +
+	    '<img src="' + result['image'] + '" class="img">' +
+	    '<p>' + result['title'] + '</p>' +
+	    '<p><small>' + result['date'] +
+	    '</small></p></a>';
+		console.log(result);
+		//$('#project' + idNumber + '.detailsImage').html(projectHTML + result.summary);
+		$(".details").html(projectHTML + result['summary']);
+	}
